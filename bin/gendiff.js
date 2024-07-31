225bbafbaf48c163cf3b0f2b070e1130fbba79e7
Darya Litvinova <lit.darya876@gmail.com>
2024-07-31 13:28:08 +0200

    add options to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,8 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output usage information')
-  .helpOption('-h, --help', 'output the version number');
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output the version number')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
